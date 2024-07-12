c535f145f765ba67ddac6e5ee41f7d1fa5d1b3ff
xandaoxl <barrosxloficial@gmail.com>
2024-07-12 10:59:20 -0300

    add api

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ btn.addEventListener('click', () => {
     fetch('https://63e7c552ac3920ad5be36b95.mockapi.io/produtos')
         .then(response => response.json())
         .then(data => {
-            output.innerHTML = data.value;
+            data.forEach(item => {
+                output.innerHTML += `
+                    <img width="300" src="${item.img}" alt="${item.titulo}" />
+                    <p>${item.titulo}</p>
+                    <div style="display: flex; align-items: center; justify-content: flex-start; gap: 4px;">
+                        <s>${item.valor}</s>
+                        <p>${item.desconto}</p>
+                    </div>
+                `;
+            });
         });
 });
